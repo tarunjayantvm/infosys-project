8e391f430f578d32cfcb6ad67a8a390197f74f85
tarunjayantvm <tarunjayantvm@gmail.com>
2025-12-05 12:38:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -376,5 +376,6 @@ def editor():
     return render_template('editor.html')
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
